fc5cc6f98397d730681c39e2e12f1e11207302ba
Oskar <vaernil@gmail.com>
2017-08-15 02:48:24 +0200

    update script

--- .scripts/webuserscipts/change_github_favicon.js
+++ .scripts/webuserscipts/change_github_favicon.js
@@ -10,7 +10,8 @@ favicon_link_html.rel = 'icon';
 favicon_link_html.href = 'http://robbyonrails.com/files/google-favicon.ico';
 favicon_link_html.type = 'image/x-icon';
 
-try { 
-  document.getElementsByTagName('head')[0].appendChild( favicon_link_html ); 
+try
+{
+	document.getElementsByTagName('head')[0].appendChild( favicon_link_html );
 }
 catch(e) { }
